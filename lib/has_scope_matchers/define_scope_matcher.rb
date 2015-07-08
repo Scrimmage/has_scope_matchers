6d6c056e8c3a5b383e68aadc357ef3bd16fdc0b6
Nick DeLuca <nickdeluca08@gmail.com>
2015-07-08 14:03:16 -0500

    finish basic matcher

--- lib/has_scope_matchers/define_scope_matcher.rb
+++ lib/has_scope_matchers/define_scope_matcher.rb
@@ -8,14 +8,19 @@ module HasScopeMatchers
     class DefineScopeMatcher
 
       def initialize(scope_name)
-        @expected_scope_name = scope_name
+        @scope_name = scope_name
       end
 
       def matches?(subject)
         @subject = subject
 
-        @failure_message = "Test Fail"
-        false
+        scope_exists? &&
+          as_correct? &&
+          type_correct? &&
+          only_correct? &&
+          if_correct? &&
+          unless_correct? &&
+          allow_blank_correct?
       end
 
       def failure_message
@@ -30,6 +35,137 @@ module HasScopeMatchers
         "Tests the scopes defined by the has_scope gem for rails controllers"
       end
 
+      def as(value)
+        @as = value
+        self
+      end
+
+      def type(value)
+        @type = value
+        self
+      end
+
+      def only(value)
+        @only = value
+        self
+      end
+
+      def except(value)
+        @except = value
+        self
+      end
+
+      def unless(condition)
+        @unless_condition = condition
+        self
+      end
+
+      def if(condition)
+        @if_condition = condition
+        self
+      end
+
+      def allow_blank(value)
+        @allow_blank = value
+        self
+      end
+
+      private
+
+      def scopes_configuration
+        @subject.scopes_configuration
+      end
+
+      def configuration
+        scopes_configuration[@scope_name]
+      end
+
+      def scope_exists?
+        if configuration
+          @negated_failure_message = "#{@subject} defines #{@scope_name} when it should not"
+          true
+        else
+          @failure_message = "#{@subject} does not define #{@scope_name}"
+          false
+        end
+      end
+
+      def as_correct?
+        return true if @as.nil?
+
+        if configuration[:as] != @as
+          @failure_message = "expected scope to be defined as #{@as}, got #{configuration[:as] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
+
+      def type_correct?
+        return true if @type.nil?
+
+        if configuration[:type] != @type
+          @failure_message = "expected scope to have type #{@type}, got #{configuration[:type] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
+
+      def only_correct?
+        return true if @only.nil?
+
+        if configuration[:only] != [ @only ]
+          @failure_message = "expected scope to only allow #{@only} actions, got #{configuration[:only] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
+
+      def except_correct?
+        return true if @except.nil?
+
+        if configuration[:except] != [ @except ]
+          @failure_message = "expected scope to not allow #{@except} actions, got #{configuration[:except] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
+
+      def if_correct?
+        return true if @if_condition.nil?
+
+        if configuration[:if] != @if_condition
+          @failure_message = "expected if condition #{@if_condition}, got #{configuration[:if] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
+
+      def unless_correct?
+        return true if @unless_condition.nil?
+
+        if configuration[:unless] != @unless_condition
+          @failure_message = "expected unless condition #{@unless_condition}, got #{configuration[:unless] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
+
+      def allow_blank_correct?
+        return true if @allow_blank.nil?
+
+        if configuration[:allow_blank] != @allow_blank
+          @failure_message = "expected allow_balnk to be #{@allow_blank}, got #{configuration[:allow_blank] || 'nil'}"
+          false
+        else
+          true
+        end
+      end
     end
   end
 end
--- spec/has_scope_matchers_spec.rb
+++ spec/has_scope_matchers_spec.rb
@@ -1,11 +1,39 @@
 require 'spec_helper'
 
 describe HasScopeMatchers do
+
+  class TreesController < ActionController::Base
+    has_scope :color, :unless => :show_all_colors?
+    has_scope :only_tall, :type => :boolean, :only => :index, :if => :restrict_to_only_tall_trees?
+    has_scope :shadown_range, :default => 10, :except => [ :index, :show, :new ]
+    has_scope :root_type, :as => :root, :allow_blank => true
+    has_scope :planted_before, :default => proc { Date.today }
+    has_scope :planted_after, :type => :date
+    has_scope :calculate_height, :default => proc {|c| c.session[:height] || 20 }, :only => :new
+    has_scope :paginate, :type => :hash
+    has_scope :args_paginate, :type => :hash, :using => [:page, :per_page]
+    has_scope :categories, :type => :array
+    has_scope :title, :in => :q
+    has_scope :content, :in => :q
+    has_scope :conifer, type: :boolean, :allow_blank => true
+
+    has_scope :only_short, :type => :boolean do |controller, scope|
+      scope.only_really_short!(controller.object_id)
+    end
+
+    has_scope :by_category do |controller, scope, value|
+      scope.by_given_category(controller.object_id, value + "_id")
+    end
+  end
+
+  subject { TreesController }
+
   it 'has a version number' do
     expect(HasScopeMatchers::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  it { is_expected.to_not define_scope(:trees_are_not_cool) }
+  it { is_expected.to define_scope(:color).unless(:show_all_colors?) }
+  it { is_expected.to define_scope(:only_tall).type(:boolean).only(:index).if(:restrict_to_only_tall_trees?) }
+  it { is_expected.to define_scope(:root_type).as(:root).allow_blank(true) }
 end
